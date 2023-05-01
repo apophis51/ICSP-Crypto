4d802eeb2c79818ad1a2eb96e3acc04cf70b2ba0
apophis51 <malcolmxvernon@hotmail.com>
2023-05-01 15:41:14 -0400

    version 1

--- src/custom_greeting_frontend/src/index.jsx
+++ src/custom_greeting_frontend/src/index.jsx
@@ -11,13 +11,40 @@ import { custom_greeting_backend } from "../../declarations/custom_greeting_back
 import { custom } from "../../declarations/custom_greeting_backend";
 import {useState} from "react";
 
+const Footer = () => {
+  return (
+    <footer className="footer p-10 bg-neutral text-neutral-content">
+  <div>
+    <span className="footer-title">Services</span> 
+    <a className="link link-hover">Branding</a>
+    <a className="link link-hover">Design</a>
+    <a className="link link-hover">Marketing</a>
+    <a className="link link-hover">Advertisement</a>
+  </div> 
+  <div>
+    <span className="footer-title">Company</span> 
+    <a className="link link-hover">About us</a>
+    <a className="link link-hover">Contact</a>
+    <a className="link link-hover">Jobs</a>
+    <a className="link link-hover">Press kit</a>
+  </div> 
+  <div>
+    <span className="footer-title">Legal</span> 
+    <a className="link link-hover">Terms of use</a>
+    <a className="link link-hover">Privacy policy</a>
+    <a className="link link-hover">Cookie policy</a>
+  </div>
+</footer>
+  );
+};
+
 const MyHello = () => {
   const [name, setName] = React.useState('');
   const [message, setMessage] = React.useState('');
   const [inputValue, setInput] = useState("");
   const [balanceResult, setBalance] = useState("");
   const [isDisabled, setDisabled] = useState(false);
-  const [buttonText, setText] = useState("Get Tokens!");
+  const [buttonText, setText] = useState("Get 10,000 Free Tokens!");
   const [recipientId, setId] = useState("")
   const [amount, setAmount] = useState("");
   const [isTransferDisabled, setTransferDisabled] = useState(false);
@@ -55,122 +82,71 @@ const MyHello = () => {
   }
 
   return (
-    <div style={{ "fontSize": "30px" }}>
-      <div style={{ "backgroundColor": "yellow" }}>
-        <p>Greetings, from ICPS!</p>
-        <p>
+    <div style={{ "fontSize": "30px" }} className = "flex-col px-96 text-5xl">
+      <div style={{ "backgroundColor": "grey" }} className = "justify-center">
+        <p className="text-red-300 text-center"><b>ICSP - Insane Clown Shiba Posse Coin - Beta Edition</b></p>
+        <p className="text-red-300 text-center">
           <img src="/OIG.png" />
-          Check your Account Token Balance.
         </p>
       </div>
-      <div style={{ margin: "30px" }}>
+      <div style={{ margin: "30px" }} className = "text-center">
+        <p>Check your Account Token Balance.</p>
         <input
+        
           id = "balance-principal-id"
+          className="input input-bordered w-full max-w-xs"
           type = "text"
           placeholder="Enter a Principal ID"
           value = {inputValue}
           onChange={(e) => setInput(e.target.value)}
           />
           <br></br>
-          <button onClick={handleClick}
+          <button className ="btn"
+          onClick={handleClick}
                   >Get Balance!</button>
           
           <br></br>
-        <button onClick={handleFaucet}
+        <button className ="btn" onClick={handleFaucet}
         disabled = {isDisabled}>{buttonText}</button>
         <br></br>
-        <p>This account has a balance of{balanceResult}</p>
+        <p>This account has a balance of: </p>
+        <br></br>
+        <p>{balanceResult}</p>
         <br></br>
         <legend>Account to Transfer To:</legend>
-        <input type = "text"  value = {recipientId} onChange={(e)=> setId(e.target.value)}/>
+        <input type = "text"  
+        className="input input-bordered w-full max-w-xs"
+        value = {recipientId} 
+        onChange={(e)=> setId(e.target.value)}/>
         <legend>Transfer Amount:</legend>
         <input 
+        className="input input-bordered w-full max-w-xs"
         type = "number"  
         value = {amount} 
         onChange = {(e) => setAmount(e.target.value)}
         disabled = {isTransferDisabled}/>
-        <button onClick = {handleTransfer}>Transfer</button>
+        <br></br>
+        <button className ="btn" onClick = {handleTransfer}>Transfer</button>
         <p hidden={isHidden}>{feedback}</p>
       </div>
+      <Footer />
     </div>
   );
 };
 
-const init = async () => {
 
-
-const authClient = await AuthClient.create();
-
-await authClient.login({
-  identityProvider: "https://identity.ic0.app/#authorize",
-  onSuccess: async () => {
-    render(<MyHello />, document.getElementById("app"));
-  }
-});
-}
-init();
+// const init = async () => {
+// const authClient = await AuthClient.create();
+// await authClient.login({
+//   identityProvider: "https://identity.ic0.app/#authorize",
+//   onSuccess: async () => {
+    render(
+    <MyHello />, document.getElementById("app")
+    );
+//   }
+// });
+// }
+// init();
 //--------------------------------------------------------------------------------------------------
 
 
-// import * as React from "react";
-// import { render } from "react-dom";
-// import { custom_greeting_backend } from "../../declarations/custom_greeting_backend";
-
-// const MyHello = () => {
-//   const [name, setName] = React.useState('');
-//   const [message, setMessage] = React.useState('');
-
-//   async function doGreet() {
-//     const greeting = await custom_greeting_backend.greet(name);
-//     setMessage(greeting);
-//   }
-
-//   return (
-//     <div style={{ "fontSize": "30px" }}>
-//       <div style={{ "backgroundColor": "yellow" }}>
-//         <p>Greetings, from DFINITY!</p>
-//         <p>
-//           {" "}
-//           Type your message in the Name input field, then click{" "}
-//           <b> Get Greeting</b> to display the result.
-//         </p>
-//       </div>
-//       <div style={{ margin: "30px" }}>
-//         <input
-//           id="name"
-//           value={name}
-//           onChange={(ev) => setName(ev.target.value)}
-//         ></input>
-//         <button onClick={doGreet}>Get Greeting!</button>
-//       </div>
-//       <div>
-//         Greeting is: "
-//         <span style={{ color: "blue" }}>{message}</span>"
-//       </div>
-//     </div>
-//   );
-// };
-
-// render(<MyHello />, document.getElementById("app"));
-
-
-
-// import { custom_greeting_backend } from "../../declarations/custom_greeting_backend";
-
-// document.querySelector("form").addEventListener("submit", async (e) => {
-//   e.preventDefault();
-//   const button = e.target.querySelector("button");
-
-//   const name = document.getElementById("name").value.toString();
-
-//   button.setAttribute("disabled", true);
-
-//   // Interact with foo actor, calling the greet method
-//   const greeting = await custom_greeting_backend.greet(name);
-
-//   button.removeAttribute("disabled");
-
-//   document.getElementById("greeting").innerText = greeting;
-
-//   return false;
-// });
